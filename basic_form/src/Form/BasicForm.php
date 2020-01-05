72e585dd572641981fe2b85145b413acce26e72e
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2020-01-05 15:24:10 +0530

    Update BasicForm.php

--- basic_form/src/Form/BasicForm.php
+++ basic_form/src/Form/BasicForm.php
@@ -5,54 +5,69 @@ namespace Drupal\basic_form\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
+/**
+ * Implements a basic form.
+ */
 class BasicForm extends FormBase {
 
-    public function getFormId()
-    {
-        return 'basic_form';
-    }
+  /**
+   * {@inheritDoc}
+   */
+  public function getFormId() {
+    return 'basic_form';
+  }
 
-    public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state)
-    {
-        $form['name'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Enter your name'),
-            '#required' => TRUE,
-        ];
-        $form['birthdate'] = [
-            '#type' => 'date',
-            '#title' => $this->t('Enter your birthdate'),
-            '#required' => TRUE,
-        ];
-        $form['age'] = [
-            '#type' => 'tel',
-            '#title' => $this->t('Enter your age'),
-            '#required' => TRUE,
-        ];
-        $form['gender'] = [
-            '#type' => 'select',
-            '#title' => $this->t('Choose your Gender'),
-            '#options' => array(
-                '' => $this->t('Choose'),
-                'Male' => $this->t('Male'),
-                'Female' => $this->t('Female'),
-                'Other' => $this->t('Other'),
-            ),
-            '#required' => TRUE,
-        ];
-        $form['submit'] = [
-            '#type' => 'submit',
-            '#value' => $this->t('Submit'),
-        ];
+  /**
+   * {@inheritDoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    // Create text input for name.
+    $form['name'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Enter your name'),
+      '#required' => TRUE,
+    ];
+    // Create date input for birthdate.
+    $form['birthdate'] = [
+      '#type' => 'date',
+      '#title' => $this->t('Enter your birthdate'),
+      '#required' => TRUE,
+    ];
+    // Create numeric input for name.
+    $form['age'] = [
+      '#type' => 'tel',
+      '#title' => $this->t('Enter your age'),
+      '#required' => TRUE,
+    ];
+    // Create text input for name.
+    $form['gender'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Choose your Gender'),
+      '#options' => [
+        '' => $this->t('Choose'),
+        'Male' => $this->t('Male'),
+        'Female' => $this->t('Female'),
+        'Other' => $this->t('Other'),
+      ],
+      '#required' => TRUE,
+    ];
+    // Create submit button.
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
 
-        return $form;
-    }
+    return $form;
+  }
 
-    public function submitForm(array &$form,FormStateInterface $form_state)
-    {
-        $this->messenger()->addStatus($this->t('Your name is @name', ['@name'=>$form_state->getValue('name')]));
-        $this->messenger()->addStatus($this->t('Your birthdate is @birth', ['@birth'=>$form_state->getValue('birthdate')]));
-        $this->messenger()->addStatus($this->t('Your age is @age', ['@age'=>$form_state->getValue('age')]));
-        $this->messenger()->addStatus($this->t('Your gender is @gender', ['@gender'=>$form_state->getValue('gender')]));
-    }
+  /**
+   * {@inheritDoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->messenger()->addStatus($this->t('Your name is @name', ['@name' => $form_state->getValue('name')]));
+    $this->messenger()->addStatus($this->t('Your birthdate is @birth', ['@birth' => $form_state->getValue('birthdate')]));
+    $this->messenger()->addStatus($this->t('Your age is @age', ['@age' => $form_state->getValue('age')]));
+    $this->messenger()->addStatus($this->t('Your gender is @gender', ['@gender' => $form_state->getValue('gender')]));
+  }
+
 }
